0bb12b1343ceed2dcf6de7d179da159e3e08b777
Adam Poliak <azpoliak@gmail.com>
2022-09-29 15:00:04 -0400

    week 04: contains.java

--- week04/Contains.java
+++ week04/Contains.java
@@ -1,10 +1,23 @@
-class Contains {
+// Author: Adam Poliak
+// Date: 09/29
+// Function to check if a string contains a character
+
+public class Contains {
   
   public static boolean contains(String phrase, char c) {
     // your code here
     // algorithm: go through each character in phrase 
     //     for each character, check whether it is equal to c
-    return true;
+    int count = 0; // keep track of how many times we see the character c
+    for (int i = 0; i < phrase.length(); i++) {
+      char currentChar = phrase.charAt(i);
+      if (currentChar == c) {
+        //return true;
+        count += 1;
+      }
+    }
+    return count > 0;
+    //return false;
   }
   
   public static void main(String[] args) {

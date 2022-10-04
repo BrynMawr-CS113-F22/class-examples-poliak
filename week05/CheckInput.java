d4e038868cb734a55c380bb8880bd21568aaf233
Adam Poliak <azpoliak@gmail.com>
2022-10-04 12:47:30 -0400

    checkInput stub: week 5

---
+++ week05/CheckInput.java
@@ -0,0 +1,16 @@
+public class CheckInput {
+
+
+    public static boolean isInteger(String input) {
+
+    }
+
+    public static void main(String[] args) {
+        isInteger("1234");   //
+        isInteger("1.234"); //
+        isInteger("abc");  //
+        isInteger("12s");  //
+
+    }
+    
+}

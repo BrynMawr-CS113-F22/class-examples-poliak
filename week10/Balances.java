acd5dcfa3d2466981c4c6e6dad8ba32c97d14495
Adam Poliak <azpoliak@gmail.com>
2022-11-10 12:25:10 -0500

    lecture 17: starter code

---
+++ week10/Balances.java
@@ -0,0 +1,16 @@
+class Balances {
+  
+  public static void main(String[] args) {
+
+    // read in csv file
+    // sort the customers
+    // selectionSort(customers);
+    // print the customers
+  }
+
+  public static void swap(int i, int j, Customer[] L) {
+  }
+  
+  public static void selectionSort(Customer[] L) {
+  }
+}
---
+++ week10/Customer.java
@@ -0,0 +1,22 @@
+class Customer {
+
+  private String name;
+  private double balance;
+  
+  public Customer(String name, double balance) {
+    this.name = name;
+    this.balance = balance;
+  }  
+
+  public String getName() {
+    return this.name;
+  }
+
+  public double getBalance() {
+    return this.balance;
+  }
+
+  public String toString() {
+    return this.name+" "+this.balance;
+  }
+}
